60592517a46890e162a4f717353e659b0787314e
linuxcompas <kalikeylogger00@gmail.com>
2025-09-22 14:13:19 +0200

    array på kalender

--- src/StudentKalender/Kalender.java
+++ src/StudentKalender/Kalender.java
@@ -1,4 +1,26 @@
 package StudentKalender;
 
 public class Kalender {
+    public int[] days;
+
+
+    public Kalender(int days) {
+        days[] day = new days[6];
+
+        days[0] = new days("Mandag");
+        days[1] = new days("Tirsdag");
+        days[2] = new days("Onsdag");
+        days[3] = new days("Torsdag");
+        days[4] = new days("Fredag");
+        days[5] = new days("Lørdag");
+        days[6] = new days("Søndag");
+
+
+
+
+
+
+
+
+    }
 }
