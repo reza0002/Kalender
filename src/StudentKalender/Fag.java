f32ff23bd560e4405fd6ac8efac6f293d509ec60
reza0002 <remo0002@stud.ek.dk>
2025-09-22 14:16:23 +0200

    .

--- src/StudentKalender/Fag.java
+++ src/StudentKalender/Fag.java
@@ -1,4 +1,11 @@
 package StudentKalender;
 
 public class Fag {
+    public String navn;
+    public String farve;
+
+    public Fag(String navn, String farve) {
+        this.navn = navn;
+        this.farve = farve;
+    }
 }
